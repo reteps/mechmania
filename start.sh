c9dfff4a3193df4a5a2bd057780494f7254a4cf4
Peter Stenger <peter.a.stenger@gmail.com>
2021-09-24 14:47:03 -0500

    init

---
+++ start.sh
@@ -0,0 +1,6 @@
+java -jar engine/mm27-engine.jar -n "team-honestpotatofarmers" -N "team-starter-bot" -e "python3 -u bot.py" -E "python3 -u starter_bot.py"
+
+
+mv team*.json logs/
+mv game.json logs/
+mv engine.json logs/
